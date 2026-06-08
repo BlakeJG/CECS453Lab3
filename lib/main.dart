61bebdab916d1ecdc2abbafd27374698d0405392
BlakeJG <113153978+BlakeJG@users.noreply.github.com>
2026-06-08 12:03:56 -0700

    Update main.dart
    Update to set up assignment

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,18 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello World!')
     );
   }
 }
